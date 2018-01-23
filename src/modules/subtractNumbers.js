69dd98669e93190afe7dff058bf3d85b9fda9ad3
Deandre Crayton <deandrecrayton@gmail.com>
2018-01-22 19:35:33 -0600

    completed module exercise

---
+++ src/modules/subtractNumbers.js
@@ -0,0 +1,13 @@
+import add from "./subtract";
+
+export function subtractOne(num) {
+  return add(num,1);
+}
+
+export function subtractTen(num) {
+  return add(num,10);
+}
+
+export function subtractFive(num) {
+  return add(num, 5);
+}
